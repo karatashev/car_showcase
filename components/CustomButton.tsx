53ec0a5babe175269cd777f4e2775d4cf44600a5
Vasil <95942547+karatashev@users.noreply.github.com>
2023-07-03 15:16:15 +0200

    Hero page, navbar, footer, button custom component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { CustomButtomProps } from "@/types"
+import Image from "next/image"
+
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtomProps) => {
+  return (
+    <button
+        disabled={false}
+        type={btnType || "button"}
+        className={`custom-btn ${containerStyles}`}
+        onClick={handleClick}
+    >
+
+        <span className={`flex-1`}>
+            {title}
+        </span>
+
+    </button>
+  )
+}
+
+export default CustomButton
---
+++ types/index.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtomProps {
+    title: string;
+    containerStyles?: string;
+    handleClick?:
+    MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
+}
